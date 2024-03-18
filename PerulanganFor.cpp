b94a9fe0ed1a24e2c9097c67d9d9fc6cafcc00a3
nopriterbang <novalpriya@gmail.com>
2024-03-18 15:04:19 +0700

    mengubah nama file

---
+++ PerulanganFor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int i;
+ int arr[5];
+
+ for(i = 0; i < 5; i++)
+ {
+ 
+ 
+  cout << i << " : " << "Nopri" << endl;
+ }
+  for (i = 0; i < 5; i++)
+  
+    cout << "Bilangan Nama = ";
+    cin >> arr[i];
+  }
+
+  for (i = 0; i<5; i++) {
+    cout << "Bilangan ke - " << i << " " << arr[i] << endl;
+  }
+}
